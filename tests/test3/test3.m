0761d536079e80894179776ed76029bc6f7bd180
ahmetakman <ahmet.akman@metu.edu.tr>
2023-12-19 15:14:01 +0300

    test 3 first draft

--- tests/test3/test3.m
+++ tests/test3/test3.m
@@ -1,3 +1,4 @@
+
 %%%%%%%%%%%%% ARGUMENT SETTINGS %%%%%%%%%%%%
 plot_while_scanning = false; % if true the scans will be plotted one by one. Note that this would slow down the process considerably.
 
@@ -9,13 +10,20 @@ overlap_coefficient = 1; % in case an overlap needed.
 
 center_frequency = interval(1); %for initialization
 
-num_search = 75 ; % number of search attempts
+max_angle = 360; % in degrees.
+angle_interval = 10; % e.g. change per measurement.
+
 
 %%%%%%% BASIC CALCULATIONS AND INITIALIZATIONS %%%%%%%
 
 %set the center frequency set to be swept.
 num_iter = int32(overlap_coefficient*(interval(2)-interval(1))/(sample_frequency));
 frequencies = linspace(interval(1),interval(2),num_iter);
+
+% set the angle point group. e.g. = [0 10 20 30 40 ... 355]
+num_angle = int32(max_angle/angle_interval);
+
+angle_array = linspace(0,max_angle,num_angle);
 
 % initialization of the pluto rx object.
 rxPluto = sdrrx('Pluto',...
@@ -33,21 +41,40 @@ rxPluto.EnableRFDCCorrection = true;
 rxPluto.GainSource = "Manual";
 rxPluto.Gain = 10; % To be tuned further.
 
+%%%%%%%%%5 ACTIVE RUN %%%%%%%%%%
 
-%%% HOPPING SEARCH %%%
-for i = 1:num_search
-[Av, FREQ] = search(rxPluto,frequencies,sample_frequency,num_iter,plot_while_scanning);
+[Av, detected_freq] = search(rxPluto,frequencies,sample_frequency,num_iter,plot_while_scanning);
+
 if(Av>-40)
 disp(Av);
-disp(FREQ);
+disp(detected_freq);
 else
 disp("Not yet detected")
 end
 pause(0.01)
-disp("waited")
+
+gains_per_angle = [];
+for j = 1:num_angle
+[Av,FREQ] = scan(rxPluto,detected_freq,sample_frequency);
+
+if(Av>-40)
+    gains_per_angle = [gains_per_angle Av];
+else
+    angle_array(j) = []
+end
 end
 
-
+%%%%%%%% Estimate the angle %%%%%%
+mean_gain = mean(gains_per_angle);
+filtered_logical = gains_per_angle > mean_gain;
+estimated_angle = mean(filtered_logical * angle_array);
+%%%%%%%% Plot the profile %%%%%%%%
+figure;
+plot(angle_array, gains_per_angle);
+xlabel("Angle in degrees");
+ylabel("Gain per angle");
+grid on;
+title("Estimated Angle = ",num2str(estimated_angle));
 %%%%%%%%%%%%%%%% FUNCTION DEFINITION %%%%%%%%%%%%%%%%%%
 % Search function:
 % It sweep through the frequencies specified and returns the detected frequency for that search with its gain.
@@ -110,4 +137,31 @@ j = indices(I);
 
 detected_f = (f-sample_frequency/2)+j;
 
+end
+
+
+function [gain,detected_f] = scan(rxPluto,freq_,sample_frequency)
+
+
+    tic;
+    
+
+    
+    rxPluto.CenterFrequency = freq_;
+    
+    data = rxPluto();
+    %release(rxPluto);
+    
+    [p, f] = pspectrum(data, sample_frequency);
+    
+    
+    toc
+    
+    [M, j] = max(p);
+    
+    
+    gain = pow2db(M);
+    
+    detected_f = (freq-sample_frequency/2)+j;
+    
 end
