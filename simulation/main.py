542458be168b4be7602f40f7ea169c1455648d99
ahmetakman <ahmet.akman@metu.edu.tr>
2024-03-30 18:12:11 +0300

    sample argument parser

--- simulation/main.py
+++ simulation/main.py
@@ -5,3 +5,11 @@ import sys
 import os
 import argparse
 import time
+
+#define arguments as velocity, time, and distance
+parser = argparse.ArgumentParser(description='Simulate a car moving at a constant velocity')
+parser.add_argument('velocity', type=float, help='Velocity of the car in m/s')
+parser.add_argument('time', type=float, help='Time in seconds')
+
+
+args = parser.parse_args()
